0167eb5b5e53ff96f43c0e35b9fed28e987d40f7
Julian <julianete4@gmail.com>
2023-02-24 01:00:32 -0300

    estructura register terminada, falta capturar datos

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import './register.css'
 import WelcomeSection from '../WelcomeSection/WelcomeSection'
+import Image from '../Image/Image'
 
 export default function Register() {
-  return (
-    <div className='register'>
-        <WelcomeSection />
-        <form>
-            <fieldset>
-                <legend>Name</legend>
-                <input type='text' name='name' id='name' />
-            </fieldset>
-        </form>
-    </div>
-  )
+    return (
+        <div className='register'>
+            <div>
+                <WelcomeSection />
+                <form className='form'>
+                    <fieldset>
+                        <legend>Name</legend>
+                        <input type='text' name='name' id='name' />
+                        <Image src='./images/profile.svg' />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Email</legend>
+                        <input type='email' name='email' id='email' />
+                        <Image src='./images/@.svg' />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Password</legend>
+                        <input type='password' name='password' id='password' />
+                        <Image src='./images/lock.svg' />
+                    </fieldset>
+                    <fieldset>
+                        <legend>Confirm Password</legend>
+                        <input type='password' name='confirm-password' id='confirm-password' />
+                        <Image src='./images/lock.svg' />
+                    </fieldset>
+                    <fieldset className='notification-check'>
+                        <input type="checkbox" name='email-notification' id='email-notification' />
+                        <label htmlFor='email-notification'>Send notification to my email</label>
+                    </fieldset>
+                    <a href='#' className='sign-up'>Sign up</a>
+                    <a href='#' className='sign-in-google'><Image src='./images/Google.svg' /><span>Sign in with Google</span></a>
+                    <p>Already have an account? <a href='#' className='link'>Log in</a></p>
+                    <p>Go back to <a href='#' className='link'>home page</a></p>
+                </form>
+            </div>
+        </div>
+    )
 }
