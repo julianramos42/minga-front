b532749cee8e950f296cb1d53aaa2931c3433731
Julian <julianete4@gmail.com>
2023-02-24 19:31:51 -0300

    pagina completa y register modificado

---
+++ src/components/Index/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../Hero/Hero';
+import Register from '../Register/Register';
+
+export default function Index() {
+  return (
+    <>
+        <Hero />
+        <Register/>
+    </>
+  )
+}
