2c11eea6cb843a486dd171dd00f3c4c75be4e2ac
Julian <julianete4@gmail.com>
2023-02-27 15:07:14 -0300

    ruta Footer arreglada en MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import IndexLayout from './layouts/IndexLayout/IndexLayout';
+import MainLayout from './layouts/MainLayout/MainLayout'
 import Index from '../src/components/Index/Index';
 
 function App() {
