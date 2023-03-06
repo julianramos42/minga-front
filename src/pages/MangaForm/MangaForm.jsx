d53c3bc5d724f39a3fd6bd378739286a82fce299
Pilar890 <pilarvillarreal546@gmail.com>
2023-03-06 15:54:26 -0300

    m-07 completa

--- src/pages/MangaForm/MangaForm.jsx
+++ src/pages/MangaForm/MangaForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import H2 from '../../components/H2/H2'
 import Image from '../../components/Image/Image'
 import SendBtn from '../../components/SendBtn/SendBtn'
@@ -9,48 +9,51 @@ import axios from 'axios';
 
 export default function CreateManga() {
     const [categorias, setCategorias ] = useState([])
-    const [selectValue, setSelectValue] = useState([]);
+    const [categoria, setCategoria ] = useState(null)
     let title = useRef();
     let category = useRef();
     let description = useRef();
     let coverPhoto = useRef();
 
+    const isDisabled = categoria == null;
+
     async function handleSubmit(e) {
         e.preventDefault();
+
+        if (isDisabled) {
+          alert("falta categoria");
+          return;
+        }
+
+        const filteredCategory = categorias.find((category) => (category.name == categoria))
         let manga = {
             title: title.current.value,
-            category: category.current.value,
+            categoria: categoria,
             description: description.current.value,
-            coverPhoto: coverPhoto.current.value,
-            
+            cover_photo: coverPhoto.current.value,
+            category_id: filteredCategory._id     
         };
-        console.log(manga);
+        console.log({filteredCategory});
         const url = 'http://localhost:8080/mangas'
+        let token = localStorage.getItem('token')
+        let headers = { headers: { 'Authorization': `Bearer ${token}` } }
+        console.log(manga)
     try {
-        await axios.post(url, manga)
+        await axios.post(url, manga, headers)
         alert("Manga created successfully")
-            // e.target.reset()
-            console.log(setSelectValue())
+            e.target.reset()
     
     } catch (error) {
         console.log(error)
         console.log("ocurrio un error")
     }
     }
-    const handleSelectChange = (event) => {
-        setSelectValue(console.log(event.target.value));
-        
-    };
     
-    
-    useEffect( ()=>  { 
-        axios.get ('http://localhost:8080/categorias').then((response) => 
-        { setCategorias(response.data) } )
-        
-
-        
-    }, [] ) 
-    // console.log(categorias)
+    async function renderCategory () {
+       await axios.get ('http://localhost:8080/mangas').then((response) => 
+        { setCategorias(response.data.categories) } )
+    }
+    console.log(categorias)
 
 return (
     <div className='author'>
@@ -61,14 +64,9 @@ return (
             </section>
             <form className='author-form' onSubmit={handleSubmit}>
                     <input className='author-input' type='text' placeholder='Insert title' ref={title} />
-                    <select className='author-input' id='selectMove' ref={category} value={selectValue} onChange={handleSelectChange}>
+                    <select className='author-input' id='selectMove' ref={category} onClick={renderCategory} onChange={(e)=> setCategoria(e.target.value)}>
                     <option value=''> Insert category</option>
-                        {categorias.map(categoria =>  <option key={categoria.value} value={categoria.value}>{categoria.name}</option>
-                        )}
-                        {/* <option value='action'>Action</option>
-                        <option value='adventure'>Adventure</option>
-                        <option value='comedy'>Comedy</option>
-                        <option value='drama'>Drama</option> */}
+                        {categorias.map(categoria =>  <option key={categoria.name} value={categoria.name}>{categoria.name}</option>)}
                     </select>
                     <input className='author-input' type='text' placeholder='Insert description' ref={description} />
                     <input className='author-input' type='text' placeholder='Insert cover photo' ref={coverPhoto} />
