7c26db5bfe62fdead89b193804070898eb09bed5
Julian <julianete4@gmail.com>
2023-03-07 14:39:52 -0300

    empezando estructura

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Index from "./Index/Index";
 import AuthForm from "./AuthForm/AuthForm";
 import ChaptherForm from "./ChaptherForm/ChaptherForm";
 import AuthorForm from './AuthorForm/AuthorForm'
+import Comics from './Comics/Comics'
 
 import { createBrowserRouter } from "react-router-dom";
 
@@ -25,7 +26,8 @@ export const router = createBrowserRouter([
             { path: '/register' , element: <AuthForm state='register'/> },
             { path: '/signin' , element: <AuthForm state='login'/> },
             { path: '/chapther-form/:manga_id' , element: <ChaptherForm /> },
-            { path: '/author-form' , element: <AuthorForm /> }
+            { path: '/author-form' , element: <AuthorForm /> },
+            { path: '/comics/:page' , element: <Comics /> }
         ]
     }
 ])
